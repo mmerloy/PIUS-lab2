7f57e328ccf08b552478d117006e90584b32b07c
mmerloy <a.mmerloy@gmail.com>
2022-05-11 00:37:09 +0300

    version 2.0 . output of all fields of one article

--- u8191102-lab2/app/Http/Controllers/ArticleController.php
+++ u8191102-lab2/app/Http/Controllers/ArticleController.php
@@ -7,11 +7,18 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    public function getAll(ArticleFilter $request)
+    public function getAllArticles(ArticleFilter $request)
     {
       
         $articles = Article::with('tags')->filter($request)->paginate(20);
 
         return view('articles' , compact('articles'));
+    }
+
+    public function getAllFields($id)
+    {
+        $article = Article::query()->find($id);
+
+        return view('articles_content', compact('article'));
     }
 }
